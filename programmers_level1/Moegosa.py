4d92ab2ca2b389952dcd418426e169d135e9bc5e
SeungHwanLee <lshwan0420@gmail.com>
2020-11-24 18:36:49 +0900

    모의고사

---
+++ programmers_level1/Moegosa.py
@@ -0,0 +1,85 @@
+# 문제 설명
+# 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다.
+# 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+#
+# 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+# 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+# 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+#
+# 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때,
+# 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+#
+# 제한 조건
+# 시험은 최대 10,000 문제로 구성되어있습니다.
+# 문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+# 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+
+# [1,2,3,4,5] -> [1]
+# [1,3,2,4,2] -> [1,2,3]
+
+# 100 / 100
+def solution(answers):
+    answer = []
+    temp = [0, 0, 0]
+
+    supoja1 = [1, 2, 3, 4, 5]
+    supoja2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    supoja3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    for idx in range(len(answers)):
+        if answers[idx] == supoja1[idx % len(supoja1)]:
+            temp[0] += 1
+        if answers[idx] == supoja2[idx % len(supoja2)]:
+            temp[1] += 1
+        if answers[idx] == supoja3[idx % len(supoja3)]:
+            temp[2] += 1
+
+    for key, value in enumerate(temp):
+        if value == max(temp):
+            answer.append(key+1)
+    return sorted(answer)
+
+# 다른 사람 풀이 내가 할려했지만 귀찮아서 하지않음에도 불구하고 100점을 맞아서 하지않은 요약문
+
+
+def solution(answers):
+    p = [[1, 2, 3, 4, 5],
+         [2, 1, 2, 3, 2, 4, 2, 5],
+         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    s = [0] * len(p)
+
+    for q, a in enumerate(answers):
+        for i, v in enumerate(p):
+            if a == v[q % len(v)]:
+                s[i] += 1
+    return [i + 1 for i, v in enumerate(s) if v == max(s)]
+
+
+# cycle 함수 이용 방법 체감속도 지림
+
+
+from itertools import cycle
+
+def solution(answers):
+    giveups = [
+        cycle([1, 2, 3, 4, 5]),
+        cycle([2, 1, 2, 3, 2, 4, 2, 5]),
+        cycle([3, 3, 1, 1, 2, 2, 4, 4, 5, 5])
+    ]
+    scores = [0, 0, 0]
+    for num in answers:
+        for i in range(3):
+            if next(giveups[i]) == num:
+                scores[i] += 1
+    highest = max(scores)
+
+    return [i + 1 for i, v in enumerate(scores) if v == highest]
+
+
+print(solution([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]))
+print(solution([1, 3, 2, 4, 2, 1, 3, 2, 4, 2, 1, 3, 2, 4, 2, 1, 3, 2, 4, 2]))
+print(solution([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+print(solution([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]))
+print(solution([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]))
+print(solution([4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]))
+print(solution([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]))
