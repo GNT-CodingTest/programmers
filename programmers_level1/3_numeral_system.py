a767ec2a84dc5c210dc36bf6d38506c8a43d0c81
syk050 <syk050@naver.com>
2020-11-25 23:06:56 +0900

    3진접

--- programmers_level1/3_numeral_system.py
+++ programmers_level1/3_numeral_system.py
@@ -1,9 +1,10 @@
-def solution(n):
+def solution2(n):
 
     temp = []
     while n != 1:
-        # q, r = (n/3, n%3)
-        n, r = divmod(n, 3)
+        # n, r = (n/3, n%3)
+        # n, r = divmod(n, 3)
+        n, r = (n // 3, n % 3)
         temp.append(r)
     temp.append(n)
 
@@ -11,11 +12,51 @@ def solution(n):
     temp_len = len(temp)
     for i in range(temp_len):
         if temp[i] != 0:
-            ans = temp[i] * 3**(temp_len - i - 1)
+            ans = temp[i] * 3 ** (temp_len - i - 1)
             answer += ans
 
     return answer
 
 
-print(solution(45))
-print(solution(125))
+def solution3(n):
+    temp = []
+    while n > 2:
+        temp.append(n % 3)
+        n = n // 3
+    temp.append(n)
+    print(temp)
+
+    answer = 0
+    e = len(temp) - 1
+    for i in range(len(temp)):
+        if temp[i] != 0:
+            answer += (temp[i] * 3 ** e)
+
+        e -= 1
+
+    return answer
+
+
+def solution4(n):
+    temp = []
+    while n > 3:
+        temp.append(n % 3)
+        n = n // 3
+    temp.append(n)
+    print(temp)
+
+    answer = 0
+    e = len(temp) - 1
+    for i in range(len(temp)):
+        if temp[i] != 0:
+            answer += (temp[i] * 3 ** e)
+
+        e -= 1
+
+    return answer
+
+
+# print(solution3(6234))
+# print(solution4(6234))
+print(solution3(9))
+print(solution4(9))
