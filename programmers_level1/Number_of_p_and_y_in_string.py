9edff58cf05e1c9cefcdd2c3201532481e7340bf
SeungHwanLee <lshwan0420@gmail.com>
2020-11-27 22:40:02 +0900

    문자열 내 p와 y의 개수 다이어트 시킴

--- programmers_level1/Number_of_p_and_y_in_string.py
+++ programmers_level1/Number_of_p_and_y_in_string.py
@@ -15,7 +15,7 @@
 
 # 100 / 100
 def solution(s):
-    return True if s.lower().count('p') == s.lower().count('y') else False
+    return s.lower().count('p') == s.lower().count('y')
 
 
 print(solution('pPoooyY'))
