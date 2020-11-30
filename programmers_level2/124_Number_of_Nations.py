78696171704b2964642630f4469306c7fd642cfc
syk050 <syk050@naver.com>
2020-12-01 01:54:23 +0900

    124 나라의 숫자

--- programmers_level2/124_Number_of_Nations.py
+++ programmers_level2/124_Number_of_Nations.py
@@ -16,3 +16,24 @@ def solution(n):
 
 for i in range(1, 20):
     print(solution(i))
+
+
+def change124(n):
+    num = ['1', '2', '4']
+    answer = ""
+
+
+    while n > 0:
+        n -= 1
+        answer = num[n % 3] + answer
+        n //= 3
+
+    return answer
+
+
+def change124(n):
+    if n<=3:
+        return '124'[n-1]
+    else:
+        q, r = divmod(n-1, 3)
+        return change124(q) + '124'[r]
