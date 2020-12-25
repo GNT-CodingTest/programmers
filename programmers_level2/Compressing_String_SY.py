dc2c5ea63d68d8f81b136eefed095ec8a8af3ab8
syk050 <syk050@naver.com>
2020-12-25 19:45:48 +0900

    문자열 압축(다른사람코드)

--- programmers_level2/Compressing_String_SY.py
+++ programmers_level2/Compressing_String_SY.py
@@ -31,7 +31,7 @@ def solution(s):
             else:
                 compact_str += char
 
-        print(f'step {step}: {compact_str} => len: {len(compact_str)}')
+        # print(f'step {step}: {compact_str} => len: {len(compact_str)}')
 
         if min_len > len(compact_str):
             min_len = len(compact_str)
@@ -39,9 +39,38 @@ def solution(s):
     return min_len
 
 
-print(solution('aabbaccc'))
-print(solution('ababcdcdababcdcd'))
-print(solution('abcabcdede'))
-print(solution('abcabcabcabcdededededede'))
-print(solution('xababcdcdababcdcd'))
-print(solution('a'))
+# print(solution('aabbaccc'))
+# print(solution('ababcdcdababcdcd'))
+# print(solution('abcabcdede'))
+# print(solution('abcabcabcabcdededededede'))
+# print(solution('xababcdcdababcdcd'))
+# print(solution('a'))
+
+
+def compress(text, tok_len):
+    words = [text[i:i+tok_len] for i in range(0, len(text), tok_len)]
+    res = []
+    cur_word = words[0]
+    cur_cnt = 1
+    for a, b in zip(words, words[1:] + ['']):
+        if a == b:
+            cur_cnt += 1
+        else:
+            res.append([cur_word, cur_cnt])
+            cur_word = b
+            cur_cnt = 1
+    return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res)
+
+
+def solution2(text):
+    return min(compress(text, tok_len) for tok_len in list(range(1, int(len(text)/2) + 1)) + [len(text)])
+    # for tok_len in list(range(1, int(len(text)/2) + 1)) + [len(text)]:
+    #     compress(text, tok_len)
+
+
+print(solution2('aabbaccc'))
+print(solution2('ababcdcdababcdcd'))
+print(solution2('abcabcdede'))
+print(solution2('abcabcabcabcdededededede'))
+print(solution2('xababcdcdababcdcd'))
+print(solution2('a'))
