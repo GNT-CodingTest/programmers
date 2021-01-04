4da1d1abaec9b1a5b8e7dffe88bd8c051dcc90e6
syk050 <syk050@naver.com>
2021-01-05 00:04:39 +0900

    큰 수 만들기

--- programmers_level2/Make_a_big_number.py
+++ programmers_level2/Make_a_big_number.py
@@ -1,54 +1,27 @@
-import itertools
-
-
-def solution1(number, k):
-    combinations = list(map(''.join, itertools.combinations(number, len(number) - k)))
-
-    return max(combinations)
-
-
 def solution(number, k):
-    num_list = list(map(int, number))
-    _len = len(number) - k
 
-    max_num = max_sieve(num_list, _len)
-    # max_num = min_sieve(num_list, k)
+    stack = [number[0]]
+    for num in number[1:]:
+        while len(stack) > 0 and num > stack[-1]:
+            if k > 0:
+                stack.pop()
+                k -= 1
+            else:
+                break
 
-    return ''.join(list(map(str, max_num)))
+        stack.append(num)
 
+    if k > 0:
+        stack = stack[:-k]
 
-def max_sieve(num_list, max_len):
-    max_num = []
-    idx = num_list.index(max(num_list))
-
-    temp = []
-    if max_len <= len(num_list[idx:]):
-        del (num_list[:idx])
-        temp = min_sieve(num_list, len(num_list) - max_len)
-    else:
-        max_num.extend(num_list[idx:])
-        del (num_list[idx:])
-        temp = max_sieve(num_list, max_len - len(max_num))
-
-    if max_num:
-        temp.extend(max_num)
-
-    return temp
-
-
-def min_sieve(num_list, del_cnt):
-    cnt = 0
-    while 1:
-        if del_cnt == cnt:
-            break
-
-        idx = num_list.index(min(num_list))
-        del(num_list[idx])
-        cnt += 1
-
-    return num_list
+    return ''.join(stack)
 
 
 print(solution('1924', 2))
 print(solution('1231234', 3))
 print(solution('4177252841', 4))
+print(solution('12', 1))
+print(solution('102', 2))
+print(solution('989898', 3))
+print(solution('87654321', 3))
+
